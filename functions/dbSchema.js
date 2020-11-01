e020884264b420ab7e0b7b1d0dd4b897f53f46e8
Alex <frezoul.alex@gmail.com>
2020-11-01 11:45:27 +0100

    backend: add likes to the stories

--- functions/dbSchema.js
+++ functions/dbSchema.js
@@ -1,11 +1,38 @@
 let db = {
-  (stories: [
+  users: [
+    {
+      userId: "djkhjqsifahau9268348",
+      email: "user@email.com",
+      handle: "user",
+      createdAt: "2019-03-15T10:59:53.798Z",
+      imageUrl: "image/dskfskhsgsdf/kqfhkj",
+      bio: "Hello, my name is user, nice to meet you",
+      website: "https://user.com",
+      location: "London, UK",
+    },
+  ],
+  stories: [
     {
       userHandle: "user",
       body: "This is a story",
       createdAt: "2020-10-30T11:25:47.624Z",
       likeCount: 5,
       commentCount: 2,
-    }
-  ]),
+    },
+  ],
+};
+
+const userDetails = {
+  // Redux data
+
+  credentials: {
+    userId: "djkhjqsifahau9268348",
+    email: "user@email.com",
+    handle: "user",
+    createdAt: "2019-03-15T10:59:53.798Z",
+    imageUrl: "image/dskfskhsgsdf/kqfhkj",
+    bio: "Hello, my name is user, nice to meet you",
+    website: "https://user.com",
+    location: "London, UK",
+  },
 };
--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,15 @@
 const { getAllStories, postOneStory } = require("./handlers/stories");
-
 const functions = require("firebase-functions");
 const app = require("express")();
-
 const FBAuth = require("./utility/fbAuth");
 
-const { signup, login } = require("./handlers/users");
+const {
+  signup,
+  login,
+  uploadImage,
+  addUserDetails,
+  getAuthenticatedUser,
+} = require("./handlers/users");
 
 // Stories routes
 app.get("/stories", getAllStories);
@@ -14,5 +18,8 @@ app.post("/story", FBAuth, postOneStory);
 // Users routes
 app.post("/signup", signup);
 app.post("/login", login);
+app.post("/user/image", FBAuth, uploadImage);
+app.post("/user", FBAuth, addUserDetails);
+app.get("/user", FBAuth, getAuthenticatedUser);
 
 exports.api = functions.region("europe-west1").https.onRequest(app);
